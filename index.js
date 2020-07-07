c211e2f8318b1144a0a6ff3822aa7f79ea55b9c9
John Pendergast <jpending@gmail.com>
2020-07-07 16:54:36 -0400

    completed shopping list app

--- index.js
+++ index.js
@@ -8,6 +8,35 @@ function funkyList() {
 
         const listItem = $('.js-shopping-list-entry').val();
 
+        //checks if input field is not an empty string//
+        if (listItem !== '') {
+            $('.shopping-list').append(`<li>
+          <span class="shopping-item">${listItem}</span>
+          <div class="shopping-item-controls">
+            <button class="shopping-item-toggle">
+              <span class="button-label">check</span>
+            </button>
+            <button class="shopping-item-delete">
+              <span class="button-label">delete</span>
+            </button>
+          </div>
+        </li>`);
+            //adds new li with user input and buttons//
+            $('.js-shopping-list-entry').val('');
+            //clears form after submission//
+        }
+    });
+
+    //toggles item to be checked using closest li to button container//
+    $('.shopping-list').on('click', '.shopping-item-toggle', function (event) {
+        $(this).closest('.shopping-item-controls').siblings('.shopping-item').toggleClass('shopping-item__checked');
+    });
+
+
+    //deletes closest li to  buttons container//
+    $('.shopping-list').on('click', '.shopping-item-delete', function (event) {
+        $(this).closest('li').remove();
+    });
 }
 
 
