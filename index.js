fab6a38649176818b8887f701e2f7364c80a8370
John Pendergast <jpending@gmail.com>
2020-07-07 16:37:09 -0400

    completed some js

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function funkyList() {
+
+    //activates when information is submitted//
+    $('#js-shopping-list-form').submit(function (event) {
+        event.preventDefault();
+
+        const listItem = $('.js-shopping-list-entry').val();
+
+}
+
+
+
+
+$(funkyList);
